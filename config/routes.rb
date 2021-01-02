96a33491c3acb24066bd04d8487a3c49c1982174
Yasen Kostov <qskatak04@gmail.com>
2021-01-03 00:01:52 +0200

    Fix home router

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  # get 'pages/home'
   get 'pages/help'
   get 'pages/about'
+  get '', to: "pages#home"
   get '/about', to: "pages#about"
   get '/tour', to: "pages#tour"
   get '/gladiator', to: "pages#gladiator"
