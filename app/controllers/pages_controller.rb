08ba8762db7912e00756fbdc6ff1b705bce5f6a8
Yasen Kostov <qskatak04@gmail.com>
2021-01-02 23:39:01 +0200

    Add /about

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,9 @@ class PagesController < ApplicationController
   def home
   end
 
+  def about
+  end
+
   def help
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/help'
+  get 'pages/about'
+  get '/about', to: "pages#about"
 
   root to: 'pages#home'
 end
