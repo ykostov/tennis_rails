325de2e3c23f460ca90c8d82be40c3102f592067
Yasen Kostov <qskatak04@gmail.com>
2021-01-03 23:11:05 +0200

    Add scaffold posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_193139) do
+ActiveRecord::Schema.define(version: 2021_01_03_210657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,25 @@ ActiveRecord::Schema.define(version: 2021_01_03_193139) do
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
   end
 
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.bigint "admin_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["admin_id"], name: "index_posts_on_admin_id"
+  end
+
+  create_table "tours", force: :cascade do |t|
+    t.string "title"
+    t.string "body"
+    t.string "text"
+    t.bigint "admin_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["admin_id"], name: "index_tours_on_admin_id"
+  end
+
+  add_foreign_key "posts", "admins"
+  add_foreign_key "tours", "admins"
 end
