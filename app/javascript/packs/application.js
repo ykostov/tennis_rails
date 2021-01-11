ce59d4a348a0d04de362aa8a4ee8f58f6fc87cfd
Yasen Kostov <qskatak04@gmail.com>
2021-01-11 19:39:30 +0200

    Add JS

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,14 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+
+require("design/active.js")
+require("design/bootstrap.min.js")
+require("design/jquery-2.2.4.min.js")
+require("design/plugins.js")
+require("design/popper.min.js")
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
