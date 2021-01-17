4ce4a357b013d33c351a8866e436de504313c1fc
Yasen Kostov <qskatak04@gmail.com>
2021-01-17 23:37:57 +0200

    Fix TYPO

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,8 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :admins
   get 'pages/home'
-  get 'pages/help'
   get 'pages/about'
   get '/about', to: "pages#about"
-  get '/gladiator', to: "pages#gladiator"
   get '/ranklist', to: "pages#ranklist"
   root to: 'pages#home'
 end
