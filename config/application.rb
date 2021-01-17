d81b9227241d3225d7e43784043e912aaa3c321b
Rumen Milushev <r@vert.bg>
2021-01-17 18:20:08 +0200

    Add icomoon and classy nav fonts for pipeline

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Tennis
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.assets.paths << Rails.root.join("app", "assets", "fonts")
 
     # Configuration for the application, engines, and railties goes here.
     #
