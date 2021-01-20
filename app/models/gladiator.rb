0058648f7fe9e509bf62f59f9ea0bd622a744fcb
Yasen Kostov <qskatak04@gmail.com>
2021-01-20 20:49:18 +0200

    bg locales

--- app/models/gladiator.rb
+++ app/models/gladiator.rb
@@ -2,8 +2,8 @@ class Gladiator < ApplicationRecord
   belongs_to :admin
 
   def status
-    return 'In progress...' if start_date == Date.today
-    return 'Finished'       if start_date < Date.today
-    return 'Upcoming'       if start_date > Date.today
+    return 'В игра....' if start_date == Date.today
+    return 'Завършил'       if start_date < Date.today
+    return 'Предстоящ'       if start_date > Date.today
  end
 end
