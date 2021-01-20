e434238fd0afde8aca3e27f89908e1daa3f4c864
Rumen Milushev <r@vert.bg>
2021-01-20 17:59:00 +0200

    Add tour status

--- app/models/gladiator.rb
+++ app/models/gladiator.rb
@@ -1,3 +1,9 @@
 class Gladiator < ApplicationRecord
   belongs_to :admin
+
+  def status
+    return 'In progress...' if start_date == Date.today
+    return 'Finished'       if start_date < Date.today
+    return 'Upcoming'       if start_date > Date.today
+  end
 end
