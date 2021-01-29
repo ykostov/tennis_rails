82245cffb9870330c25c06d4a84534d47cad0d82
Rumen Milushev <r@vert.bg>
2021-01-29 17:34:39 +0200

    Add has_many through association for player and tour

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,7 @@ class Player < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :players_in_tour
+  has_many :gladiators, through: :players_in_tour
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_24_001951) do
+ActiveRecord::Schema.define(version: 2021_01_28_173047) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,15 @@ ActiveRecord::Schema.define(version: 2021_01_24_001951) do
     t.index ["reset_password_token"], name: "index_players_on_reset_password_token", unique: true
   end
 
+  create_table "players_in_tours", force: :cascade do |t|
+    t.bigint "player_id", null: false
+    t.bigint "gladiator_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["gladiator_id"], name: "index_players_in_tours_on_gladiator_id"
+    t.index ["player_id"], name: "index_players_in_tours_on_player_id"
+  end
+
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -81,5 +90,7 @@ ActiveRecord::Schema.define(version: 2021_01_24_001951) do
 
   add_foreign_key "competitions", "admins"
   add_foreign_key "gladiators", "admins"
+  add_foreign_key "players_in_tours", "gladiators"
+  add_foreign_key "players_in_tours", "players"
   add_foreign_key "posts", "admins"
 end
