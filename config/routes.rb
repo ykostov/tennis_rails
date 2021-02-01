a326da4cf586a25610665743d0cf6d21c79b4e1e
Yasen Kostov <qskatak04@gmail.com>
2021-02-01 15:53:43 +0200

    Add template stuff ready-to-use

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get 'pages/about'
   get '/about', to: "pages#about"
   get '/ranklist', to: "pages#ranklist"
+  get '/template', to: "pages#template"
   root to: 'pages#home'
 end
